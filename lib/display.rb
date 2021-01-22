16668f271bc474e30db3e2c71e52683c91ff9544
qpongratz <lagarathan@gmail.com>
2021-01-22 14:51:53 -0600

    Refactor display

--- lib/display.rb
+++ lib/display.rb
@@ -1,33 +1,55 @@
 # frozen_string_literal: true
 
-
+# Displays the board
 module Display
 
-  PIECES = ['⚪', '⚫']
+  PIECES = ['⚪', '⚫'].freeze
 
   def display_board(board)
     readable_board = prepare_board(board)
-    print "\n┌"
-    (board.board_width - 1).times {print '──┬'}
-    print '──┐'
+    print_top(board)
     readable_board.each_with_index do |row, index|
-      row.map! { |piece| piece = piece == '  ' ? piece : PIECES[piece - 1] }
-      print "\n"
-      row.each { |piece| print "│#{piece}" }
-      print "│\n├"
-      if index == readable_board.length - 1
-        (row.length - 1).times { print '──┴' }
-      else
-        (row.length - 1).times { print '──┼' }
-      end
-      print '──┤'
+      convert_ids_to_pieces(row)
+      print_row_with_pieces(row)
+      index == readable_board.length - 1 ? print_bottom(row) : print_spacer(row)
     end
     print "\n\n\n"
   end
+
+  private
 
   def prepare_board(board)
     board_copy = board.board_state.map(&:dup)
     board_copy.each { |array| array.push('  ') until array.length == board.board_height }
     board_copy.transpose.reverse
-  end  
+  end
+
+  def print_top(board)
+    print "\n┌"
+    (board.board_width - 1).times {print '──┬'}
+    print '──┐'
+  end
+
+  def convert_ids_to_pieces(row)
+    row.map! { |piece| piece == '  ' ? piece : PIECES[piece - 1] }
+  end
+
+  def print_row_with_pieces(row)
+    print "\n"
+    row.each { |piece| print "│#{piece}" }
+    print '│'
+  end
+
+  def print_spacer(row)
+    print "\n├"
+    (row.length - 1).times { print '──┼' }
+    print '──┤'
+  end
+
+  def print_bottom(row)
+    print "\n├"
+    (row.length - 1).times { print '──┴' }
+    print '──┤'
+  end
+
 end
